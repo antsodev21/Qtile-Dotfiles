f8baa77da2dbdc39b1deee478bb3eee0ba000089
Antonio Martínez Ramón <antsoftware2107@proton.me>
2025-09-18 20:12:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Instalando dependencias
 sudo apt install rofi volumeicon-alsa cbatticon thunar network-manager-applet feh scrot dunst arandr qtile kitty
+#Eliminando anterior configuracion
 sudo rm -rf ~/.config/qtile
+#Copiando Archivos
 cp -r ~/Qtile-Dotfiles/qtile ~/.config
 cp -r ~/Qtile-Dotfiles/rofi ~/.config
+#Dando Permisos al Autostart y creando carpeta de Wallpapers
 chmod +x ~/.config/qtile/autostart.sh
 mkdir ~/Wallpapers
